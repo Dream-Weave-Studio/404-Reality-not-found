8ebccd5daccb63442a9b47018b52ab33d5a406ab
NicolasCanuto88 <nicolascanuto88@gmail.com>
2025-07-19 21:20:02 +0200

    Initial Commit

---
+++ Assets/Cameras/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam2_5D;
+    public CinemachineVirtualCamera vcamIso;
+
+    public enum ViewMode { Side2_5D, Isometric }
+    public ViewMode currentMode = ViewMode.Side2_5D;
+
+    void Start()
+    {
+        SwitchView(currentMode); // Imposta visuale iniziale
+    }
+
+    void Update()
+    {
+        // Toggle visuale con BackQuote (`)
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            ViewMode newMode = currentMode == ViewMode.Side2_5D ? ViewMode.Isometric : ViewMode.Side2_5D;
+            SwitchView(newMode);
+        }
+    }
+
+    public void SwitchView(ViewMode newMode)
+    {
+        currentMode = newMode;
+
+        vcam2_5D.Priority = (newMode == ViewMode.Side2_5D) ? 10 : 0;
+        vcamIso.Priority = (newMode == ViewMode.Isometric) ? 10 : 0;
+
+        Debug.Log($"Visuale attuale: {currentMode}");
+    }
+}
