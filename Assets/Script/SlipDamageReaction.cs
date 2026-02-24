69daf2ed076d3af96709c338d3437ba01f3c57e7
NicolasCanuto88 <nicolascanuto88@gmail.com>
2026-02-24 12:42:50 +0100

    ChangeNamePlayer

---
+++ Assets/Script/SlipDamageReaction.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Reazione che applica danno e rabbia UNA SOLA VOLTA.
+/// Da usare insieme a InteractableObject o come trigger.
+/// </summary>
+public class SlipDamageReaction : InteractionReaction
+{
+    [Header("Parametri")]
+    [SerializeField] private float damageAmount = 15f;
+    [SerializeField] private float rageAmount = 10f;
+    [SerializeField] private bool giveRage = true;
+
+    private bool hasActivated = false;
+
+    protected override void PerformReaction(GameObject interactor)
+    {
+        if (hasActivated) return;
+
+        hasActivated = true;
+
+        GameEntity entity = interactor.GetComponent<GameEntity>();
+        if (entity != null)
+        {
+            // Applica effetti
+            entity.TakeDamage(damageAmount);
+            // if (giveRage) entity.ModifyRage(rageAmount);
+
+            Debug.Log($"[SlipDamage] Scivolata! Danno: {damageAmount}, Rabbia: {(giveRage ? rageAmount : 0)}");
+        }
+    }
+}
